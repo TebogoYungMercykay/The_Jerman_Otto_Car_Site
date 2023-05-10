fa6e4714747cc148108f2873ad4f59bae879211e
Selepe Sello <sbkskhalo.kq@gmail.com>
2023-05-10 13:34:41 +0200

    Login and Logout API functionality

---
+++ COS216/PA4/php/login.php
@@ -0,0 +1,31 @@
+<!-- Selepe Sello u20748052 -->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include_once("head.php"); ?>
+    </head>
+    <body class="login-signup">
+        <?php include_once("header.php"); ?>
+        <section id="logo" class="mt-5">
+            <div class="name-logo-top">
+                <h1 class="page-name-purpose" id="page-name-brands">Login to your Jerman <img class="logo-top-img-2" src="../img/logo.jpg" alt="Jerman Otto"/>tto Account</h1>
+                <hr>
+            </div>
+        </section>
+        <div class="logoContainer" id="div-with-form">
+            <div class="logo">
+                <form class="main-form-login-signup" method="post" action="validate-login.php">
+                    <label>Email:&nbsp;&nbsp;&nbsp;</label>
+                    <input id="login-email" type="email" name="email" required autocomplete="off"><br><br>
+                    <label>Password:</label>
+                    <input id="login-password" type="password" name="password" required><br><br>
+                    <!-- Buttons -->
+                    <input id="login-submit" type="submit" name="submit" value="Login">&nbsp;&nbsp;
+                    <input id="submit-find-car" type="reset" name="reset" value="Reset Form" class="btnFindCarResults">&nbsp;&nbsp;
+                    <input id="signup-button" type="button" name="button" value="Go To Sign Up" onclick="location.href='signup.php';">
+                </form>
+            </div>
+        </div>
+        <?php include_once("footer.php"); ?>
+    </body>
+</html>
